3603f1d3fd8438d3586dcef439c3e6bbb021bff9
fallen8 <sorksoek2@gmail.com>
2023-07-14 19:00:45 +0900

    문제 모으는 중

--- app/frontend/srcs/app/page.tsx
+++ app/frontend/srcs/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <main className="relative w-full h-screen">
-      <div className="relative w-full h-full flex justify-center items-center">
+      <div className="relative flex items-center justify-center w-full h-full">
         <div className="relative max-w-5xl w-full max-h h-auto bg-[#f3f3f3] shadow-lg font-noto-sans-kr">
-          <h1 className=" bg-blue-500 text-2xl text-white font-bold text-center py-2">소리로 맞히는 거상 테스트</h1>
-          <div className="relative w-full py-20 flex justify-center items-center">
-            <button className=" px-10 py-4 rounded-sm bg-white hover:text-white hover:bg-blue-500  font-semibold duration-200 transition-colors">
+          <h1 className="py-2 text-2xl font-bold text-center text-white bg-blue-500 ">소리로 맞히는 거상 테스트</h1>
+          <div className="relative flex items-center justify-center w-full py-20">
+            <button className="px-10 py-4 font-semibold transition-colors duration-200 bg-white rounded-sm hover:text-white hover:bg-blue-500">
               시작하기
             </button>
           </div>
