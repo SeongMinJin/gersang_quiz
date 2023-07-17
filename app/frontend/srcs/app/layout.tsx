9882d2f84363000e5bbe8611f2be748e0148cf41
fallen8 <sorksoek2@gmail.com>
2023-07-17 20:54:24 +0900

    구글 애드센스 삽입

--- app/frontend/srcs/app/layout.tsx
+++ app/frontend/srcs/app/layout.tsx
@@ -14,17 +14,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-				<link href="https://fonts.cdnfonts.com/css/helvetica-neue-55" rel="stylesheet"></link>
-				<link href="https://fonts.cdnfonts.com/css/trial-oceanic" rel="stylesheet"></link>
-				<link rel="preconnect" href="https://fonts.googleapis.com"></link>
-				<link rel="preconnect" href="https://fonts.gstatic.com"></link>
-				<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet"></link>
-				<link rel="preconnect" href="https://fonts.googleapis.com"></link>
-				<link rel="preconnect" href="https://fonts.gstatic.com"></link>
-				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
-			</head>
+        <link href="https://fonts.cdnfonts.com/css/helvetica-neue-55" rel="stylesheet"></link>
+        <link href="https://fonts.cdnfonts.com/css/trial-oceanic" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2554957326810515" crossOrigin="anonymous"></script>
+      </head>
       <body>{children}</body>
-      
+
     </html>
   )
 }
