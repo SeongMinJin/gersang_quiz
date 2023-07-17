ecaa53fe617bc0181ff3d792c64014efbb24439d
fallen8 <sorksoek2@gmail.com>
2023-07-17 21:14:39 +0900

    통계 추가

--- app/frontend/srcs/app/layout.tsx
+++ app/frontend/srcs/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 
+import { Analytics } from '@vercel/analytics/react';
+
 export const metadata: Metadata = {
   title: '거상 고인물 테스트',
   description: '거상 고인물 테스트',
@@ -24,7 +26,10 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2554957326810515" crossOrigin="anonymous"></script>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
 
     </html>
   )
